f77ef003a31427217eb34433637c99401eb02161
Likhin M N <mnlikhin@gmail.com>
2026-06-24 10:57:09 +0530

    fix: sqflite import error

--- lib/core/transfer_history.dart
+++ lib/core/transfer_history.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_common_ffi.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
 import 'package:beam/ui/state/app_state.dart';
 
